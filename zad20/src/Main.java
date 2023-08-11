91eb5a74780865eb1e4baf318207d95ae47d6247
Paweł Jonik <p.jonik2005@gmail.com>
2023-08-11 13:43:32 +0200

    zad20

---
+++ zad20/src/Main.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> ls=new ArrayList<>();
+        for(int i=0; i<grades.size(); i++) {
+            int x=grades.get(i);
+            if(x>=38 && (x+5-x%5)-x<3) {
+                ls.add(x+5-x%5);
+            }else {
+                ls.add(x);
+            }
+        }
+        return ls;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
